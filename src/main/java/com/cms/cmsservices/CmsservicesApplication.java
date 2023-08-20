e5bdb7462c226d0920f0d0b34412ed83c23e9639
pabbarajupavan <pavankumar24@live.com>
2023-08-20 22:10:17 +0530

    project created

---
+++ src/main/java/com/cms/cmsservices/CmsservicesApplication.java
@@ -0,0 +1,13 @@
+package com.cms.cmsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsservicesApplication.class, args);
+	}
+
+}
